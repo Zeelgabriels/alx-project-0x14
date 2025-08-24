b6934dfc51e6d58cdf23ae6f4d7994c80c76664d
zeelgabriels <zealgabriels@gmail.com>
2025-08-24 22:22:45 +0100

    Task 2: Set up Header, Footer, Layout and landing page

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default Layout;
---
+++ alx-movie-app/fontawesome.ts
@@ -0,0 +1,5 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+// Tell FontAwesome not to auto-add CSS (we’ll handle it manually)
+config.autoAddCss = false;
