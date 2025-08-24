f4308da629d8dd19f3946f0c54650a415c8689a6
zeelgabriels <zealgabriels@gmail.com>
2025-08-24 21:06:06 +0100

    Task 1: Bootstrap MovieApp

--- alx-movie-app/components/layouts/Footer.tsx
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-900 text-white p-4">
-      <p>&copy; {new Date().getFullYear()} CineSeek. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} alx-movie-app. All rights reserved.</p>
     </footer>
   );
 };
--- alx-movie-app/components/layouts/Header.tsx
+++ alx-movie-app/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <header className="bg-gray-900 text-white p-4">
-      <h1>CineSeek</h1>
+      <h1>alx-movie-app</h1>
     </header>
   );
 };
